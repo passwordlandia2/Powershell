69f928513d3d5132115962fe243925714573ea22
passwordlandia2 <jamiller8686@outlook.com>
2024-09-13 12:24:01 -0700

    Add files via upload
    A fun way to practice my operators and have some fun with it

---
+++ AnimeShowFun.ps1
@@ -0,0 +1,9 @@
+##this is a fun script to imitate the Mashle song's chorus that Tik Tok keeps showing.
+ 
+"Who The Best " + "I'm the Best!" + "The Beeeeeeest!"
+$next = "To The Next! " 
+$next2 = "the nischta nescht"
+$next + "To " + $next2
+$bb = "Bling " + "Bang " *2
+$bb2 = $bb*3 + "Bang " * 2 + "Born "
+"Now Singing " + $bb2 *3 +  $next + $next2
